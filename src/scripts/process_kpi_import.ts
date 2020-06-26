7ab785105982953b0ab9123307396aa5bc1242f6
perovserg@mail.ru <perovserg@mail.ru>
2020-06-27 00:19:43 +0300

    refactoring import v.2

--- src/scripts/process_kpi_import.ts
+++ src/scripts/process_kpi_import.ts
@@ -37,8 +37,22 @@ async function main() {
     }
 
     let rootProcess = await transactionalEntityManager.findOne(Process, { where: { name: 'BizCase' } });
+    if (rootProcess) {
+      const options = { where: { parentId: rootProcess.id }, relations: ['kpi_libs'] };
+      const processes = await transactionalEntityManager.find(Process, options);
+      for (const { kpi_libs } of processes) {
+        const updatedKpiLibs = kpi_libs.map(kpi => ({ ...kpi, process: null }));
+        await transactionalEntityManager.save(KpiLib, updatedKpiLibs);
+      }
+      await transactionalEntityManager.delete(Process,{ parentId: rootProcess.id });
+      await transactionalEntityManager.delete(Process,{ id: rootProcess.id });
+      rootProcess = null;
+    }
     if (!rootProcess) {
-      rootProcess = await transactionalEntityManager.save(Process, new Process({ name: 'BizCase' }));
+      rootProcess = await transactionalEntityManager.save(Process, new Process({
+        name: 'BizCase',
+        industry: foundIndustry,
+      }));
     }
 
     for (const processName of processesData) {
