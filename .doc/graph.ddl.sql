5aa19141cbc7bb566e9319db893d92afe1b736fc
Vlad <gargalyk.vladislav@gmail.com>
2020-07-15 01:35:42 +0300

    add procedure creation graph for one company

--- .doc/graph.ddl.sql
+++ .doc/graph.ddl.sql
@@ -1,4 +1,7 @@
 drop graph if exists capgraph cascade;
+
+create graph capgraph;
+
 set graph_path=capgraph;
 
 create vlabel company;
@@ -11,3 +14,52 @@ create elabel has_subsidiary inherits (has_partner);
 create elabel has_supplier inherits (has_partner);
 
 create elabel has_child;
+
+CREATE TABLE cap._tmp_import_company_graph
+(
+    cid character varying(100) NOT NULL,
+    json_data json NOT NULL,
+    imported boolean NOT NULL DEFAULT false,
+    log text,
+    last_import_date timestamp without time zone,
+    PRIMARY KEY (cid)
+)
+WITH (
+    OIDS = TRUE
+);
+
+ALTER TABLE cap._tmp_import_company_graph
+    OWNER to agens;
+
+create or replace function import_company_graph(i_cid character varying) returns character varying as
+$$
+declare
+  v_json_graph json;
+  v_json_start json;
+  v_json_end json;
+  v_json_rel json;
+  v_log text;
+  rec record;
+begin
+  v_log := '';
+  for rec in (
+    select json_array_elements(json_extract_path(json_array_elements(json_extract_path(p.js, '_fields')), 'segments')) json_graph
+      from (
+        	SELECT json_array_elements(g.json_data) js FROM cap._tmp_import_company_graph g where g.cid=i_cid
+     ) p)
+   loop
+     v_json_graph := rec.json_graph;
+	 v_json_start := json_extract_path(v_json_graph, 'start');
+	 v_json_end := json_extract_path(v_json_graph, 'end');
+	 v_json_rel := json_extract_path(v_json_graph, 'relationship');
+
+	 /*
+	 MERGE (E:person {name: 'Edward'})-[L:likes]->(A:person {name: 'Alice'});
+	 */
+
+
+   end loop;
+
+  return v_log;
+end;
+$$ language 'plpgsql';
