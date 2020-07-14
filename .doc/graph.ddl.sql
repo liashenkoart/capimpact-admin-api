3664cec5c0b6eba8e233a833bd117ee8503ca5e1
Vlad <gargalyk.vladislav@gmail.com>
2020-07-14 20:15:15 +0300

    last changes

--- .doc/graph.ddl.sql
+++ .doc/graph.ddl.sql
@@ -1,10 +1,13 @@
 drop graph if exists capgraph cascade;
-create graph capgraph;
+set graph_path=capgraph;
 
-create vlabel Capability;
+create vlabel company;
+create vlabel company_type;
 
-create vlabel Company;
+create elabel has_partner;
+create elabel has_direct_invest inherits (has_partner);
+create elabel has_strateg_alliance inherits (has_partner);
+create elabel has_subsidiary inherits (has_partner);
+create elabel has_supplier inherits (has_partner);
 
-create vlabel Industry;
-
-create vlabel Process;
+create elabel has_child;
