6c9997a72b82f76ba53c197f31a3282527195900
evgeny_gromov <eg.exceedteam@gmail.com>
2020-07-20 15:42:17 +0300

    Add multi cids queries

--- src/modules/caps/services/company.graph.service.ts
+++ src/modules/caps/services/company.graph.service.ts
@@ -30,21 +30,36 @@ export class CompanyGraphService {
 
   @Transactional()
   async findPartnerNetworksByCid(cid: string, hps: any): Promise<any> {
+    let cidsArr = [];
+    if (cid.includes('&')) {
+      cidsArr = cid.split('&');
+    }
+    const cids = cidsArr.join(', ').replace(/^(\w+), (\w+)$/i, `['$1', '$2']`);
     const nhops=+hps.query.nhops;
+
     const edges = this.persistenceManager
       .query<any>(
-        new QuerySpecification<any>(`match p=(n:company {cid: $cid})-[*..${nhops}]->() with edges(p) as e return distinct e;`)
-          .bind({
-            cid
-          })
+        new QuerySpecification<any>(
+          cidsArr.length === 0
+            ? `match p=(n:company {cid: $cid})-[*..${nhops}]->() with edges(p) as e return distinct e;`
+            : `match p=(n:company)-[*..${nhops}]->() where n.cid in ${cids} with edges(p) as e return distinct e;`
       )
+          .bind(cidsArr.length === 0 ? {
+             cid
+          } : null)
+      );
+
     const nodes = this.persistenceManager
       .query<any>(
-        new QuerySpecification<any>(`match p=(n:company {cid: $cid})-[*..${nhops}]->() with nodes(p) as nd return distinct nd;`)
-          .bind({
+        new QuerySpecification<any>(
+          cidsArr.length === 0
+          ? `match p=(n:company {cid: $cid})-[*..${nhops}]->() with nodes(p) as nd return distinct nd;`
+          : `match p=(n:company)-[*..${nhops}]->() where n.cid in ${cids} with nodes(p) as nd return distinct nd;`
+        )
+          .bind(cidsArr.length === 0 ? {
             cid
-          })
-      )
+          } : null)
+      );
 
     return Promise.all([nodes, edges])
       .then(records =>
