7543f1785c537fbc5222fa2008a1ec17b1b2f129
gukanzoadze <gukanzoadze@gmail.com>
2020-08-20 05:12:42 +0400

    fix: lens and company view crash

--- src/modules/caps/services/lense.service.ts
+++ src/modules/caps/services/lense.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, HttpService } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository,FindManyOptions } from 'typeorm';
 
 import { Lense } from '../entities';
 import { LensesArgs } from '../dto';
@@ -10,12 +10,25 @@ export class LenseService {
   constructor(@InjectRepository(Lense) private readonly lenseRepository: Repository<Lense>) {}
 
   async findAll(args: LensesArgs): Promise<Lense[]> {
+    // const options = this.getFindAllQuery(args);
+
     let result = await this.lenseRepository.find({
-      where: args,
       order: {id: 'ASC'},
+      ...args,
       relations: ["classifications"]
     });
 
     return result
+  }
+
+  getFindAllQuery(query: LensesArgs): FindManyOptions {
+    const { page, skip, limit, ...params } = query;
+    let where: any = params;
+
+    return {
+      skip: (page - 1) * limit,
+      take: limit,
+      where,
+    };
   }
 }
