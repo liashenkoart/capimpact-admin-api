dd06da12b4661ffce982683af408f11d6155a597
liashenkoart <liashenkoart@gmail.com>
2020-10-22 15:41:37 -0700

    refactored function that is used toconvert query params

--- src/modules/common/services/base.service.ts
+++ src/modules/common/services/base.service.ts
@@ -3,12 +3,18 @@ import { fromPairs } from 'lodash';
 
 export abstract class BaseService {
   protected getFindAllQuery(query: any): FindManyOptions {
-    const { page, skip, limit, sort, ...where } = query;
-    const transformedWhere = {};
-    Object.entries(where).map(([key, value]) => {
-      const isTemplate = typeof value == 'string' && (value.startsWith('%') || value.endsWith('%'));
-      transformedWhere[key] = isTemplate ? Raw(alias => `${alias} ILIKE '${value}'`) : value;
-    });
+    const { page, skip, limit, sort, where } = query;
+    const transformedWhere = [];
+    if (where) {
+      Object.entries(where).map(([key, value]) => {
+        const isTemplate =
+          typeof value == 'string' && (value.startsWith('%') || value.endsWith('%'));
+        transformedWhere.push({
+          [key]: isTemplate ? Raw(alias => `${alias} ILIKE '${value}'`) : value,
+        });
+      });
+    }
+
     return {
       skip: skip > 0 ? skip : (page - 1) * limit,
       take: limit,
