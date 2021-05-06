d5a4e33d8e6d0731cce49e53e997d6415e9a417d
Cloud <cloudcrowd1016@gmail.com>
2021-05-06 15:24:38 -0700

    adjust ingest caplibs script

--- src/scripts/caplibs_from_xlsx.ts
+++ src/scripts/caplibs_from_xlsx.ts
@@ -25,15 +25,15 @@ async function main() {
           const libName = row['Oil And Natural Gas Properties'];
           const existsLib = await transactionalEntityManager.findOne(CapabilityLib,{ where: { name: libName  }});
           if(existsLib) {
-            result.push({ id: existsLib.id, status: 'exists', name: libName});
+            result.push({ id: existsLib.id, name: libName, status: 'exists'});
           } else {
             const newLib = await transactionalEntityManager.save(CapabilityLib,new CapabilityLib({ name: libName }));
             const capNodeEntity = new CapabilityTree({ cap_name: newLib.name, parent: masterRoot, capability_lib_id: newLib.id, type: 'master'});
             const newCapNodeEntity = await transactionalEntityManager.save(CapabilityTree,capNodeEntity);
-            result.push({ id: newCapNodeEntity.id, status: 'new', name: libName})
+            result.push({ id: newLib.id, name: libName, status: 'new'})
         }
         });
-        console.dir(result)
+        console.dir(result, {'maxArrayLength': null})
   });
 })
 }
